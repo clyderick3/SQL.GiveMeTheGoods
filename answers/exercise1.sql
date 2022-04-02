08135ed3dfacd742770c3013c971d322413f5882
Clyde Broderick <clyderick3@gmail.com>
2022-04-02 02:51:34 -0400

    Started and completed e1.

---
+++ answers/exercise1.sql
@@ -0,0 +1,8 @@
+mysql> Select * FROM Students WHERE City LIKE  '_a%';
++--------------+---------------+------+------------+---------+
+| StudentName  | Address       | City | PostalCode | Country |
++--------------+---------------+------+------------+---------+
+| Javale Magee | 5 Warrior Ave | Java | C2033      | Canada  |
++--------------+---------------+------+------------+---------+
+1 row in set (0.00 sec)
+
