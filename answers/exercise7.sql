0e97cbd9ca556b63f20e04d19e81cc2227bac250
Clyde Broderick <clyderick3@gmail.com>
2022-04-02 15:42:57 -0400

    Completed e7.

---
+++ answers/exercise7.sql
@@ -0,0 +1,9 @@
+mysql> SELECT * FROM Courses WHERE CreditHours BETWEEN 10 and 20;
++------------+-------------+
+| Course     | CreditHours |
++------------+-------------+
+| Java       | 10          |
+| Python     | 11          |
+| Javascript | 12          |
++------------+-------------+
+3 rows in set (0.01 sec)
