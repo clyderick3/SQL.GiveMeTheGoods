787c1facfa5e28883e894a8194aeb89e13424c53
Clyde Broderick <clyderick3@gmail.com>
2022-04-02 16:07:18 -0400

    Completed assignment.

---
+++ answers/exercise11.sql
@@ -0,0 +1,11 @@
+mysql> Select * From Students AS Learners;
++--------------+----------------+---------+------------+---------------+------+
+| StudentName  | Address        | City    | PostalCode | Country       | ID   |
++--------------+----------------+---------+------------+---------------+------+
+| Mark Jackson | 88 Fifth Ln    | Trenton | 08618      | United States | NULL |
+| Mara Johnson | 10 Twisted Ln  | Smyrna  | 19977      | United States | NULL |
+| Mike Johnson | 12 Elephant Ln | Atlanta | 40466      | United States | NULL |
+| Muna Bregee  | 40 Rephant Rd  | Eclipse | A4044      | Haiti         | NULL |
+| Javale Magee | 5 Warrior Ave  | Java    | C2033      | Canada        | NULL |
++--------------+----------------+---------+------------+---------------+------+
+5 rows in set (0.00 sec)
